ccad27423cc0775de2f566610e710f7d7706cc5c
Davis-Patterson <Davisfpatterson@gmail.com>
2024-11-02 01:10:23 -0400

    updated user name character limits

--- Accounts/models.py
+++ Accounts/models.py
@@ -35,8 +35,8 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30, blank=True, null=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
