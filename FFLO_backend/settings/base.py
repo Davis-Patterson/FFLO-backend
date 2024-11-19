031712367515b6300aa2abeb5dc2ef56c56d5f98
Davis-Patterson <Davisfpatterson@gmail.com>
2024-11-19 12:26:07 -0500

    added os.environ.setdefault

--- FFLO_backend/settings/base.py
+++ FFLO_backend/settings/base.py
@@ -7,6 +7,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTINGS_MODULE'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
