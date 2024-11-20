4538ab4dca507728e9e4becea9dd6b7296d903b5
Davis-Patterson <Davisfpatterson@gmail.com>
2024-11-20 10:21:01 -0500

    updated manage.py and __init__.py to load development environment if not in production

--- FFLO_backend/settings/__init__.py
+++ FFLO_backend/settings/__init__.py
@@ -1,6 +1,8 @@
 import os
+from decouple import config
 
-settings_module = os.getenv('DJANGO_SETTINGS_MODULE', 'FFLO_backend.settings.development')
+settings_module = config('DJANGO_SETTINGS_MODULE', 'FFLO_backend.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 if settings_module:
     module = __import__(settings_module, globals(), locals(), ['*'])
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
 
 def main():
@@ -19,4 +20,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTINGS_MODULE', default='FFLO_backend.settings.development'))
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
